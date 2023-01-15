70e8e47112e497f700dd745552926f9a71a39952
Mayur <mayurk@testvagrant.com>
2023-01-15 23:46:55 +0530

    Negative test case check

--- src/test/java/TestScenario_01.java
+++ src/test/java/TestScenario_01.java
@@ -1,11 +1,22 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestScenario_01 {
+import static io.restassured.RestAssured.given;
 
+public class TestScenario_01 {
+    //Arrange
+    //Act
+    //Assert
     @Test
-    public void testCase_001(){
-        Assert.assertEquals(1,1);
+    public void testCase_001() {
+
+        given()
+                .when()
+                .get("https://dummyapi.io/data/v1/user?limit=10")
+                .then()
+                .statusCode(403)
+                .log().body();
+
     }
 
 }
