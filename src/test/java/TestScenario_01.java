0187cd9e15266c7a46c180813ed2ddc98f553c0c
Mayur <mayurk@testvagrant.com>
2023-01-15 21:40:21 +0530

    initial commit

---
+++ src/test/java/TestScenario_01.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestScenario_01 {
+
+    @Test
+    public void testCase_001(){
+        Assert.assertEquals(1,1);
+    }
+
+}
