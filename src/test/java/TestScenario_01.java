d34e93749ba89a6f9c693d0cd518f2c5b41b9324
Mayur <mayurk@testvagrant.com>
2023-01-16 17:02:24 +0530

    Two TestCases Executing

--- src/test/java/TestScenario_01.java
+++ src/test/java/TestScenario_01.java
@@ -1,3 +1,6 @@
+import io.restassured.http.ContentType;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,14 +11,38 @@ public class TestScenario_01 {
     //Act
     //Assert
     @Test
-    public void testCase_001() {
+    public void getUserList() {
 
         given()
-                .when()
+                .header("app-id", "63c4ff107cc57e330fe64382")
+                .contentType(ContentType.JSON)
+        .when()
                 .get("https://dummyapi.io/data/v1/user?limit=10")
-                .then()
-                .statusCode(403)
-                .log().body();
+        .then()
+                .statusCode(200)
+                .log().body()
+                .body("data", Matchers.hasSize(10));
+    }
+
+    @Test
+    public void createUser(){
+
+        given()
+                .header("app-id", "63c4ff107cc57e330fe64382")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"firstName\" : \"Mayur\",\n" +
+                        "    \"lastName\" : \"Kumar\",\n" +
+                        "    \"email\" : \"MayurKrms@marvello.com\" \n" +
+                        "    }")
+        .when()
+                .post("https://dummyapi.io/data/v1/user/create")
+        .then()
+                .log().body()
+                .statusCode(200)
+                .body("id", Matchers.notNullValue())
+                .body("email", Matchers.equalToIgnoringCase("MayurKrms@marvello.com"));
+
 
     }
 
